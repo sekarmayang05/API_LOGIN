ed2ad176cb620e86b3cc9318ddc909e14331d0ea
sekarmayang05 <mayangkumala05@gmail.com>
2026-07-27 15:54:01 +0700

    Tambahkan controller login user

--- controller/userController.js
+++ controller/userController.js
@@ -48,3 +48,58 @@ async function register(req, res) {
     }
 }
 
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Email dan password wajib diisi."
+            });
+        }
+
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Email atau password salah."
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Email atau password salah."
+            });
+        }
+
+        const token = jwt.sign(
+            {   id: user.id,
+                email: user.email 
+            },
+            process.env.JWT_SECRET,
+            { 
+                expiresIn: process.env.JWT_EXPIRES
+            }
+        );
+
+        return res.status(200).json({
+            message: "Login berhasil.",
+            token
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
+
