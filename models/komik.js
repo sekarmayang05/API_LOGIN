2671b8a68b40bf9d7593b87ba789107699970277
sekarmayang05 <mayangkumala05@gmail.com>
2026-07-27 14:51:50 +0700

    Tambahkan struktur skema tabel Komik

---
+++ models/komik.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+        }
+    });
+    return Komik;
+};
+
+
