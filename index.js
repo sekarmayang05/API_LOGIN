b689045ff41cf411263e863b048ebb912f01cfff
sekarmayang05 <mayangkumala05@gmail.com>
2026-07-27 15:03:06 +0700

    Buat entry point utama dan jalankan server Express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDatabase = require('./config/db');
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', require('./routes/api'));
+
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+startServer();
